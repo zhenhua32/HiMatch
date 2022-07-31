8359818176d1337d149535f712cf721eb9ddbc38
zhenhua32 <zhenhua32@gmail.com>
2022-07-31 21:19:19 +0800

    格式化

--- helper/hierarchy_tree_statistic.py
+++ helper/hierarchy_tree_statistic.py
@@ -2,7 +2,7 @@
 # coding:utf-8
 import sys
 
-sys.path.append('../HiMatch')
+sys.path.append("../HiMatch")
 import os
 from models.structure_model.tree import Tree
 import json
@@ -10,7 +10,7 @@ import copy
 from collections import defaultdict
 from helper.configure import Configure
 
-ROOT_LABEL = 'Root'
+ROOT_LABEL = "Root"
 
 
 class DatasetStatistic(object):
@@ -21,12 +21,13 @@ class DatasetStatistic(object):
         """
         super(DatasetStatistic, self).__init__()
         self.config = config
-        self.root = Tree('Root')
+        self.root = Tree("Root")
         # label_tree => root
         self.label_trees = dict()
         self.label_trees[ROOT_LABEL] = self.root
         self.hierarchical_label_dict, self.label_vocab = self.get_hierar_relations_with_name(
-            os.path.join(config.data.data_dir, config.data.hierarchy))
+            os.path.join(config.data.data_dir, config.data.hierarchy)
+        )
         self.level = 0
         self.level_dict = dict()
         self.init_prior_prob_dict = dict()
@@ -80,20 +81,20 @@ class DatasetStatistic(object):
 
         :return:
         """
-        file = open(os.path.join(self.config.data.data_dir, self.config.data.hierarchy), 'r')
+        file = open(os.path.join(self.config.data.data_dir, self.config.data.hierarchy), "r")
         data = file.readlines()
         file.close()
         hierarcy_dict = dict()
         for line in data:
-            line = line.rstrip('\n')
-            p = line.split('parent: ')[1].split(' ')[0]
-            c = line.split('child: ')[1].split(' ')[0]
+            line = line.rstrip("\n")
+            p = line.split("parent: ")[1].split(" ")[0]
+            c = line.split("child: ")[1].split(" ")[0]
             if p not in hierarcy_dict.keys():
                 hierarcy_dict[p] = [c]
             else:
                 hierarcy_dict[p].append(c)
         print(hierarcy_dict)
-        known_label = ['Root']
+        known_label = ["Root"]
         output_lines = []
         while len(known_label):
             output_lines.append([known_label[0]] + hierarcy_dict[known_label[0]])
@@ -102,21 +103,21 @@ class DatasetStatistic(object):
                     known_label.append(i)
             known_label = known_label[1:]
         print(output_lines)
-        file = open(os.path.join(self.config.data.data_dir, self.config.data.hierarchy), 'w')
+        file = open(os.path.join(self.config.data.data_dir, self.config.data.hierarchy), "w")
         for i in output_lines:
-            file.write('\t'.join(i) + '\n')
+            file.write("\t".join(i) + "\n")
         file.close()
 
     @staticmethod
     def get_hierar_relations_with_name(taxo_file_dir):
         parent_child_dict = dict()
         label_vocab = []
-        f = open(taxo_file_dir, 'r')
+        f = open(taxo_file_dir, "r")
         relation_data = f.readlines()
         f.close()
         for relation in relation_data:
             # relation_list = relation.split()
-            relation_list = relation.rstrip('\n').split('\t')
+            relation_list = relation.rstrip("\n").split("\t")
             parent, children = relation_list[0], relation_list[1:]
             assert parent not in parent_child_dict.keys()
             parent_child_dict[parent] = children
@@ -128,7 +129,7 @@ class DatasetStatistic(object):
     def get_data_statistic(self, file_name):
         all_label_num = 0
         label_num_dict = dict()
-        f = open(file_name, 'r')
+        f = open(file_name, "r")
         data = f.readlines()
         f.close()
         count_data = len(data)
@@ -144,9 +145,9 @@ class DatasetStatistic(object):
         for sample in data:
             sample_flag = False
             sample = json.loads(sample)
-            sample_label = sample['label']
+            sample_label = sample["label"]
             all_label_num += len(sample_label)
-            doc_length_all += len(sample['token'])
+            doc_length_all += len(sample["token"])
             # sample label : list of labels
             for label in sample_label:
                 path_flag = False
@@ -155,25 +156,25 @@ class DatasetStatistic(object):
                 # level_num_dict[self.label_trees[label]._depth] += 1
 
                 if label in self.init_prior_prob_dict.keys():
-                    if label in ['CS', 'Medical', 'Civil', 'ECE', 'biochemistry', 'MAE', 'Psychology']:
+                    if label in ["CS", "Medical", "Civil", "ECE", "biochemistry", "MAE", "Psychology"]:
                         prob_dict[ROOT_LABEL][label] += 1
                         self.prior_prob_dict[ROOT_LABEL][label] += 1
-                        if 'train' in file_name or 'val' in file_name:
+                        if "train" in file_name or "val" in file_name:
                             self.total_train_prob_dict[ROOT_LABEL][label] += 1
 
                     for c in self.init_prior_prob_dict[label].keys():
                         if c in sample_label:
                             prob_dict[label][c] += 1
                             self.prior_prob_dict[label][c] += 1
-                            if 'train' in file_name or 'val' in file_name:
+                            if "train" in file_name or "val" in file_name:
                                 self.total_train_prob_dict[label][c] += 1
 
                 if label not in label_num_dict:
                     label_num_dict[label] = 1
-                    label_doc_len_dict[label] = len(sample['token'])
+                    label_doc_len_dict[label] = len(sample["token"])
                 else:
                     label_num_dict[label] += 1
-                    label_doc_len_dict[label] += len(sample['token'])
+                    label_doc_len_dict[label] += len(sample["token"])
 
                 if self.label_trees[label].num_children > 0 and not (sample_flag and path_flag):
                     # flag = False
@@ -197,15 +198,15 @@ class DatasetStatistic(object):
                 label_doc_len_dict[label] = float(label_doc_len_dict[label]) / label_num_dict[label]
 
         return {
-            'num_of_samples': count_data,
-            'average_label_num_per_sample': avg_label_num,
-            'average_doc_length_per_sample': avg_doc_len,
-            'label_num_dict': label_num_dict,
-            'average_doc_length_per_label': label_doc_len_dict,
-            'sample_end_before_leaf_nodes': sample_count_not_to_end,
-            'path_end_before_leaf_nodes': path_count_not_to_end,
-            'level_sample_number': level_num_dict,
-            'prob_dict': prob_dict
+            "num_of_samples": count_data,
+            "average_label_num_per_sample": avg_label_num,
+            "average_doc_length_per_sample": avg_doc_len,
+            "label_num_dict": label_num_dict,
+            "average_doc_length_per_label": label_doc_len_dict,
+            "sample_end_before_leaf_nodes": sample_count_not_to_end,
+            "path_end_before_leaf_nodes": path_count_not_to_end,
+            "level_sample_number": level_num_dict,
+            "prob_dict": prob_dict,
         }
 
 
@@ -220,36 +221,39 @@ def prior_probs(prob_dict):
     return prob_dict
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     configs = Configure(config_json_file=sys.argv[1])
 
     rcv1_dataset_statistic = DatasetStatistic(configs)
     # rcv1_dataset_statistic.get_taxonomy_file()
     train_statistics = rcv1_dataset_statistic.get_data_statistic(
-        os.path.join(configs.data.data_dir, configs.data.train_file))
+        os.path.join(configs.data.data_dir, configs.data.train_file)
+    )
     val_statistics = rcv1_dataset_statistic.get_data_statistic(
-        os.path.join(configs.data.data_dir, configs.data.val_file))
+        os.path.join(configs.data.data_dir, configs.data.val_file)
+    )
     test_statistics = rcv1_dataset_statistic.get_data_statistic(
-        os.path.join(configs.data.data_dir, configs.data.test_file))
+        os.path.join(configs.data.data_dir, configs.data.test_file)
+    )
 
-    print('*****TRAIN*****')
+    print("*****TRAIN*****")
     print(train_statistics)
-    print(prior_probs(train_statistics['prob_dict']))
-    print('*****val*****')
+    print(prior_probs(train_statistics["prob_dict"]))
+    print("*****val*****")
     print(val_statistics)
-    print(prior_probs(val_statistics['prob_dict']))
-    print('*****TEST*****')
+    print(prior_probs(val_statistics["prob_dict"]))
+    print("*****TEST*****")
     print(test_statistics)
-    print(prior_probs(test_statistics['prob_dict']))
+    print(prior_probs(test_statistics["prob_dict"]))
 
     # check_rcv1_level_data()
-    print('*****TOTAL*****')
+    print("*****TOTAL*****")
     print(rcv1_dataset_statistic.prior_prob_dict)
     print(prior_probs(rcv1_dataset_statistic.prior_prob_dict))
-    print('*****TOTAL TRAIN*****')
+    print("*****TOTAL TRAIN*****")
     print(rcv1_dataset_statistic.total_train_prob_dict)
     print(prior_probs(rcv1_dataset_statistic.total_train_prob_dict))
     train_probs = prior_probs(rcv1_dataset_statistic.total_train_prob_dict)
-    with open(os.path.join(configs.data.data_dir, configs.data.prob_json), 'w') as json_file:
+    with open(os.path.join(configs.data.data_dir, configs.data.prob_json), "w") as json_file:
         json_str = json.dumps(train_probs)
         json_file.write(json_str)
